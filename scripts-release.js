460ee4a448f812ee4801bc70f60092666689e9c4
Ilya Pronin <studst2016@ya.ru>
2019-12-12 17:55:26 +0300

    fixed typegen packaging

--- scripts-release.js
+++ scripts-release.js
@@ -4,14 +4,15 @@ const path = require('path');
 const copy = ([srcChunks, dstChunks]) => {     
     const [src, dst] = [srcChunks, dstChunks].map(c => path.resolve(...c));
     process.stdout.write(`${src} -> ${dst} ... `);
-    fs.copyFile(path.join(src), path.join(dst), (err) => {
-        if (err) throw err;
-        console.info('done.');
-    });
+    fs.copyFileSync(path.join(src), path.join(dst));
+    process.stdout.write('ok.\n');
 };
 
 const install = [
-    [ ['dist', 'typegen.min.js'], ['typegen', 'typegen.js'] ],
+    [ ['dist', 'typegen.min.js'], ['typegen', 'typegen.min.js'] ],
+    [ ['dist', 'typegen.js'], ['typegen', 'typegen.js'] ],
+    [ ['dist', 'typegen.js.map'], ['typegen', 'typegen.js.map'] ],
+    [ ['dist_tsc', 'typegen', 'typegen.d.ts'], ['typegen', 'typegen.d.ts'] ],
 ];
 
 install.forEach(copy);
