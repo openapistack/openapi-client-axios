50abe34b0cb408cbc718200bff379fbf86b61f45
Viljami <viljami@viljami.io>
2023-12-20 15:02:34 +0100

    Re-export some axios types for typegen

--- packages/openapi-client-axios/src/index.ts
+++ packages/openapi-client-axios/src/index.ts
@@ -2,3 +2,6 @@ import { OpenAPIClientAxios } from './client';
 export default OpenAPIClientAxios;
 export * from './client';
 export * from './types/client';
+
+// re-export axios types and utilities
+export type { Axios, AxiosInstance, AxiosRequestConfig, AxiosError } from 'axios';
